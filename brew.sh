32bb01536327faf24e27d4254340542f60c4748e
Thibault Milan <thibault.milan@vesperiagroup.com>
2015-01-08 20:34:14 +0100

    Update the list of brew stuff I need

--- brew.sh
+++ brew.sh
@@ -19,26 +19,13 @@ brew upgrade
 brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew install bash-completion
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
-
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
@@ -47,46 +34,29 @@ brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 brew install homebrew/php/php55 --with-gmp
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
+# Install Android plateform tools like `fastboot`
+brew install android-platform-tools
+# Install Mackup to restore application settings
+brew install mackup
 # Install other useful binaries.
 brew install ack
-#brew install exiv2
-brew install git
+# Install git versionning system and the helper for git-flow method.
+brew install git git-flow
+# Install imagemagick for image compression / manipulation w/ webp
+# support.
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install tree
+# Install `webkit2png` that allow in CLI to capture png for website.
 brew install webkit2png
-brew install zopfli
+# Install crypto-terrorism stuff.
+brew install gpg-agent gnupg
+brew install keybase
+# Install htop for osx.
+brew install htop-osx
+
+# Install some dev tools needed for my work.
+brew install mongodb
+brew install elasticsearch
+brew install mariadb
 
 # Install Node.js. Note: this installs `npm` too, using the recommended
 # installation method.
