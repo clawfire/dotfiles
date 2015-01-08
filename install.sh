bc450bdec35d2b48423ff198bad28ac3140c0a5b
Thibault Milan <thibault.milan@vesperiagroup.com>
2015-01-08 23:45:53 +0100

    Add the script for first install run

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install RVM
+curl -L https://get.rvm.io | bash -s stable --ruby
+rvm install 2.2.0
+rvm use --default 2.2.0
+rvm rubygems latest
+# Install oh-my-zsh
+curl -L http://install.ohmyz.sh | sh
