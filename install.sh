21b00d4ac163d5eafd2b5c39a8acb46e88acc31f
Thibault Milan <hello@thibaultmilan.com>
2018-04-25 10:00:24 +0200

    Update install.sh
    Add multi user install + bump version number

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Install RVM
-curl -L https://get.rvm.io | bash -s stable --ruby
-rvm install 2.2.0
-rvm use --default 2.2.0
+curl -L https://get.rvm.io | sudo bash -s stable --ruby
+rvm install 2.4
+rvm use --default 2.4
 rvm rubygems latest
 # Install oh-my-zsh
 curl -L http://install.ohmyz.sh | sh
